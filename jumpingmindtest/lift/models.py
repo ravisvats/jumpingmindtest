ae410190d180eebf8ae14a4ea4fd9f08b009383e
ravisvats <ravisvats@gmail.com>
2023-02-01 02:23:17 +0530

    models required for lift problem

--- jumpingmindtest/lift/models.py
+++ jumpingmindtest/lift/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Elevator(models.Model):
+    floor = models.IntegerField()
+    direction = models.CharField(max_length=10)
+    status = models.CharField(max_length=10)
+    maintenance = models.BooleanField(default=False)
+
+class Request(models.Model):
+    elevator = models.ForeignKey(Elevator, on_delete=models.CASCADE)
+    floor = models.IntegerField()
+    direction = models.CharField(max_length=10)
